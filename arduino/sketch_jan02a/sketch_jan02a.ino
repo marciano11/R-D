9a6589ee68f18d4ff44b7f621aa5215603f8d014
marciano11 <22320@ma-web.nl>
2018-01-09 16:02:22 +0100

    Add files via upload

---
+++ arduino/sketch_jan02a/sketch_jan02a.ino
@@ -0,0 +1,46 @@
+/**/int led1 = 13; // give it a name:int led2 = 12; // give it a name:int led3 = 11; // give it a name:int led4 = 10; 
+// give it a name:int led5 = 9; // give it a name:int led6 = 8; // give it a name:int led7 = 7; // give it a name:int led8 = 6; // give it a name: void setup() { 
+
+     pinMode(led1, OUTPUT);            pinMode(led2, OUTPUT);  pinMode(led3, OUTPUT);
+
+        pinMode(led4, OUTPUT);  pinMode(led5, OUTPUT);            pinMode(led6, OUTPUT); 
+
+        pinMode(led7, OUTPUT);            pinMode(led8, OUTPUT); } void loop() {      digitalWrite(led1, HIGH); // turn the LED offdelay(50);
+
+          // wait for a second    digitalWrite(led1, LOW);   // turn the LED ondelay(50); 
+
+            // wait for a second    digitalWrite(led2, HIGH); // turn the LED offdelay(50);            // wait for a second    digitalWrite(led2, LOW);   // turn the LED ondelay(50); 
+
+            // wait for a second     digitalWrite(led3, HIGH); // turn the LED offdelay(50);          // wait for a second  digitalWrite(led3, LOW);  // turn the LED offdelay(50); 
+
+            // wait for a second  digitalWrite(led4, HIGH); // turn the LED offdelay(50);           // wait for a second  digitalWrite(led4, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second  digitalWrite(led5, HIGH); // turn the LED offdelay(50);           // wait for a second  digitalWrite(led5, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second  digitalWrite(led6, HIGH); // turn the LED offdelay(50);           // wait for a second  digitalWrite(led6, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second  digitalWrite(led7, HIGH); // turn the LED offdelay(50);           // wait for a second  digitalWrite(led7, LOW);  // turn the LED offdelay(50); 
+
+            // wait for a second  digitalWrite(led8, HIGH); // turn the LED offdelay(50);         // wait for a second  digitalWrite(led8, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second//back  digitalWrite(led8, HIGH); // turn the LED offdelay(50);
+
+            // wait for a second    digitalWrite(led8, LOW);   // turn the LED ondelay(50); 
+
+            // wait for a second     digitalWrite(led7, HIGH); // turn the LED offdelay(50);  // wait for a second    digitalWrite(led7, LOW);   // turn the LED ondelay(50);
+
+            // wait for a second     digitalWrite(led6, HIGH); // turn the LED offdelay(50);  // wait for a second  digitalWrite(led6, LOW);  // turn the LED offdelay(50); 
+
+            // wait for a second  digitalWrite(led5, HIGH); // turn the LED offdelay(50);     // wait for a second  digitalWrite(led5, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second  digitalWrite(led4, HIGH); // turn the LED offdelay(50);    // wait for a second digitalWrite(led4, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second  digitalWrite(led3, HIGH); // turn the LED offdelay(50);    // wait for a second digitalWrite(led3, LOW);  // turn the LED offdelay(50); 
+
+            // wait for a second  digitalWrite(led2, HIGH); // turn the LED offdelay(50);    // wait for a second digitalWrite(led2, LOW);  // turn the LED offdelay(50);
+
+            // wait for a second  digitalWrite(led1, HIGH); // turn the LED offdelay(50);    // wait for a second digitalWrite(led1, LOW);  // turn the LED offdelay(50); 
+
+            // wait for a second}
+
+            
